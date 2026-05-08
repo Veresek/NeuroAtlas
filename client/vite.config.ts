c381a9168a81a33de59977a459c9eff67adb5d72
Veresek <filipowskimedia@gmail.com>
2026-05-09 00:49:53 +0200

    fix

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,6 +6,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+	base: "/NeuroAtlas/",
 	plugins: [
 		react(),
 		babel({ presets: [reactCompilerPreset()] }),
